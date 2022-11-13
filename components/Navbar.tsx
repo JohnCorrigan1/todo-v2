0466dc96736f7b90c1ccbb07d290685a57db0cce
John Corrigan <jcorrigan322@gmail.com>
2022-11-13 11:46:42 -0500

    if user is anonymous on logout remove all todos

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,33 @@
 import Link from "next/link";
-import { auth } from "../lib/firebase";
+import { auth, db } from "../lib/firebase";
 import { signOut } from "firebase/auth";
 import { useContext } from "react";
 import { UserContext } from "../lib/AuthContext";
+import { TodosContext } from "../lib/TodoContext";
+import { deleteDoc, doc } from "firebase/firestore";
 
 const Navbar: React.FC = () => {
+  
   const { user } = useContext(UserContext);
+  const todosContext = useContext(TodosContext);
+
+  const signOutHandler = async () => {
+    if(user?.isAnonymous){
+      todosContext.items.forEach((todo) => {
+        if(todo.uid === user.uid){
+          removeAnons(todo.todoId);
+          console.log("Removed",todo.title)
+        }
+      });
+      todosContext.removeAnons(user.uid);
+  }
+  await auth.signOut();
+  };
+
+  const removeAnons = async (id: string) => {
+    console.log("removed from firebase")
+    await deleteDoc(doc(db, "todos", id));
+  };
 
   return (
     <nav className="flex w-full bg-slate-200 shadow-sm shadow-zinc-400 font-bold">
@@ -39,7 +61,8 @@ const Navbar: React.FC = () => {
               <Link href="/signin">
                 <button
                   className="bg-sky-500 shadow-sm text-white p-2 pr-3 pl-3 rounded-md active:scale-95 hover:bg-sky-600"
-                  onClick={() => auth.signOut()}
+                  // onClick={() => auth.signOut()}
+                  onClick={signOutHandler}
                 >
                   Log Out
                 </button>
--- lib/TodoContext.tsx
+++ lib/TodoContext.tsx
@@ -1,42 +1,72 @@
 import { TodoItem } from "../models/todo";
-import { useState, createContext } from "react";
+import { useState, createContext, useContext } from "react";
+import { UserContext } from "../lib/AuthContext";
+
+
 
 type TodoContextObj = {
-    items: TodoItem[],
-    addTodo: (title: string, description: string, date: string, uid: string, id: string) => void,
-    addFromFirebase(title: string, description: string, date: string, uid: string, todoId: string): void,
-    removeTodo: (id: string) => void,
-    updateId: (id: string, oldId: string) => void
-}
+  items: TodoItem[];
+  addTodo: (
+    title: string,
+    description: string,
+    date: string,
+    uid: string,
+    id: string
+  ) => void;
+  addFromFirebase(
+    title: string,
+    description: string,
+    date: string,
+    uid: string,
+    todoId: string
+  ): void;
+  removeTodo: (id: string) => void;
+  updateId: (id: string, oldId: string) => void;
+  removeAnons: (uid: string) => void;
+};
 
 type Props = {
-    children?:
-      | React.ReactChild
-      | React.ReactChild[];
-  }; 
+  children?: React.ReactChild | React.ReactChild[];
+};
 
 export const TodosContext = createContext<TodoContextObj>({
   items: [],
   addTodo: () => {},
   addFromFirebase: () => {},
   removeTodo: (id: string) => {},
-  updateId: (id: string) => {}
+  updateId: (id: string) => {},
+  removeAnons: (uid: string) => {},
 });
 
 const TodosContextProvider: React.FC<Props> = (props) => {
   const [todos, setTodos] = useState<TodoItem[]>([]);
 
-  const addTodoHandler = (title: string, description: string, date: string, uid: string, id: string) => {
+  const addTodoHandler = (
+    title: string,
+    description: string,
+    date: string,
+    uid: string,
+    id: string
+  ) => {
     setTodos((prevTodos) => {
       return [...prevTodos, new TodoItem(title, description, date, uid, id)];
     });
   };
 
-  const addFromFirebaseHandler = (title: string, description: string, date: string, uid: string, todoId: string) => {
+  const addFromFirebaseHandler = (
+    title: string,
+    description: string,
+    date: string,
+    uid: string,
+    todoId: string
+  ) => {
     setTodos((prevTodos) => {
-      return [...prevTodos, new TodoItem(title, description, date, uid, todoId)];
+      return [
+        ...prevTodos,
+        new TodoItem(title, description, date, uid, todoId),
+      ];
     });
-  }
+  };
 
   const removeTodoHanler = (id: string) => {
     setTodos((prevTodos) => {
@@ -44,7 +74,13 @@ const TodosContextProvider: React.FC<Props> = (props) => {
     });
   };
 
-  const updateIdHanlder  = (id: string, oldId: string) => {
+  const removeAnonsTodoHandler = (id: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.uid !== id);
+    });
+  };
+
+  const updateIdHanlder = (id: string, oldId: string) => {
     setTodos((prevTodos) => {
       return prevTodos.map((todo) => {
         if (todo.todoId === oldId) {
@@ -55,16 +91,20 @@ const TodosContextProvider: React.FC<Props> = (props) => {
     });
   };
 
-
   const contextValue: TodoContextObj = {
     items: todos,
     addTodo: addTodoHandler,
     addFromFirebase: addFromFirebaseHandler,
     removeTodo: removeTodoHanler,
-    updateId: updateIdHanlder
+    updateId: updateIdHanlder,
+    removeAnons: removeAnonsTodoHandler,
   };
 
-  return <TodosContext.Provider value={contextValue}>{props.children}</TodosContext.Provider>;
+  return (
+    <TodosContext.Provider value={contextValue}>
+      {props.children}
+    </TodosContext.Provider>
+  );
 };
 
 export default TodosContextProvider;
